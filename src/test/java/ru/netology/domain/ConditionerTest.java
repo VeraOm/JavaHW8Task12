b8fe0bba374e3c504c6aa046714c33f28ce059ce
VeraOm <yakov_yooy@aol.com>
2020-04-26 17:38:09 +0600

    Исходные тексты Задачи 1

---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,50 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "'Zero temperature'; 5; -5; 0; 1",
+                    "'Last temperature'; 5; -5; 4; 5",
+                    "'Max temperature'; 5; -5; 5; 5",
+            }
+            , delimiter = ';'
+    )
+    public void increaseCurrentTemperatureTest(String name, int max, int min, int start, int expected) {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setName(name);
+        conditioner.setMaxTemperature(max);
+        conditioner.setMinTemperature(min);
+        conditioner.setCurrentTemperature(start);
+
+        conditioner.increaseCurrentTemperature();
+        assertEquals(expected, conditioner.getCurrentTemperature());
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "'Zero temperature'; 5; -5; 0; -1",
+                    "'Last temperature'; 5; -5; -4; -5",
+                    "'Min temperature'; 5; -5; -5; -5",
+            }
+            , delimiter = ';'
+    )
+    public void decreaseCurrentTemperatureTest(String name, int max, int min, int start, int expected) {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setName(name);
+        conditioner.setMaxTemperature(max);
+        conditioner.setMinTemperature(min);
+        conditioner.setCurrentTemperature(start);
+
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(expected, conditioner.getCurrentTemperature());
+
+    }
+}
