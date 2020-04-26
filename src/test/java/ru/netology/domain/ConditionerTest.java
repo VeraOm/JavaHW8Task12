a6bcca98fd0868d85f7d285ce6edc3d89a209be3
VeraOm <yakov_yooy@aol.com>
2020-04-26 17:54:56 +0600

    Добавление тестов для запуска Github Actions

--- src/test/java/ru/netology/domain/ConditionerTest.java
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -12,11 +12,13 @@ class ConditionerTest {
                     "'Zero temperature'; 5; -5; 0; 1",
                     "'Last temperature'; 5; -5; 4; 5",
                     "'Max temperature'; 5; -5; 5; 5",
+                    "'More maximum temperature'; 5; -5; 6; 1"
             }
             , delimiter = ';'
     )
     public void increaseCurrentTemperatureTest(String name, int max, int min, int start, int expected) {
         Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(0);
         conditioner.setName(name);
         conditioner.setMaxTemperature(max);
         conditioner.setMinTemperature(min);
@@ -33,11 +35,13 @@ class ConditionerTest {
                     "'Zero temperature'; 5; -5; 0; -1",
                     "'Last temperature'; 5; -5; -4; -5",
                     "'Min temperature'; 5; -5; -5; -5",
+                    "'Less minimum temperature'; 5; -5; -100; -1"
             }
             , delimiter = ';'
     )
     public void decreaseCurrentTemperatureTest(String name, int max, int min, int start, int expected) {
         Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(0);
         conditioner.setName(name);
         conditioner.setMaxTemperature(max);
         conditioner.setMinTemperature(min);
